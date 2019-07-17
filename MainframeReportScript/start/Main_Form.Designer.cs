246d34a2c6c0316c9ba7b1cfcc09fd5c6f5a6389
RossKitcher <rosskitcher12@gmail.com>
2019-07-17 09:18:31 +0100

    Add files via upload

---
+++ MainframeReportScript/start/Main_Form.Designer.cs
@@ -0,0 +1,205 @@
+﻿using System.ComponentModel;
+
+namespace start
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runReportButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseFilesButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.reportStatusProgress = new System.Windows.Forms.ProgressBar();
+            this.reportBgWorker = new System.ComponentModel.BackgroundWorker();
+            this.reportStatusLabel = new System.Windows.Forms.Label();
+            this.dataFormatButton = new System.Windows.Forms.Button();
+            this.userGuideButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // runReportButton
+            // 
+            this.runReportButton.Location = new System.Drawing.Point(286, 208);
+            this.runReportButton.Name = "runReportButton";
+            this.runReportButton.Size = new System.Drawing.Size(241, 60);
+            this.runReportButton.TabIndex = 0;
+            this.runReportButton.Text = "Run Report";
+            this.runReportButton.UseVisualStyleBackColor = true;
+            this.runReportButton.Click += new System.EventHandler(this.runReportButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xlsx";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel files (*.xls*)|*.xls*";
+            this.openFileDialog1.Title = "Browse Excel Files";
+            // 
+            // browseFilesButton
+            // 
+            this.browseFilesButton.Location = new System.Drawing.Point(72, 105);
+            this.browseFilesButton.Name = "browseFilesButton";
+            this.browseFilesButton.Size = new System.Drawing.Size(198, 49);
+            this.browseFilesButton.TabIndex = 1;
+            this.browseFilesButton.Text = "Browse Data (.xls)";
+            this.browseFilesButton.UseVisualStyleBackColor = true;
+            this.browseFilesButton.Click += new System.EventHandler(this.browseFilesButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(439, 49);
+            this.textBox1.TabIndex = 2;
+            // 
+            // reportStatusProgress
+            // 
+            this.reportStatusProgress.Location = new System.Drawing.Point(202, 308);
+            this.reportStatusProgress.Name = "reportStatusProgress";
+            this.reportStatusProgress.Size = new System.Drawing.Size(397, 23);
+            this.reportStatusProgress.TabIndex = 4;
+            // 
+            // reportBgWorker
+            // 
+            this.reportBgWorker.WorkerReportsProgress = true;
+            this.reportBgWorker.WorkerSupportsCancellation = true;
+            this.reportBgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.reportBgWorker_DoWork);
+            this.reportBgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.reportBgWorker_ProgressChanged);
+            this.reportBgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.reportBgWorker_RunWorkerCompleted);
+            // 
+            // reportStatusLabel
+            // 
+            this.reportStatusLabel.AutoSize = true;
+            this.reportStatusLabel.Location = new System.Drawing.Point(361, 285);
+            this.reportStatusLabel.Name = "reportStatusLabel";
+            this.reportStatusLabel.Size = new System.Drawing.Size(98, 20);
+            this.reportStatusLabel.TabIndex = 5;
+            this.reportStatusLabel.Text = "Not Running";
+            // 
+            // dataFormatButton
+            // 
+            this.dataFormatButton.Location = new System.Drawing.Point(598, 398);
+            this.dataFormatButton.Name = "dataFormatButton";
+            this.dataFormatButton.Size = new System.Drawing.Size(190, 40);
+            this.dataFormatButton.TabIndex = 7;
+            this.dataFormatButton.Text = "Data format";
+            this.dataFormatButton.UseVisualStyleBackColor = true;
+            this.dataFormatButton.Click += new System.EventHandler(this.dataFormatButton_Click);
+            // 
+            // userGuideButton
+            // 
+            this.userGuideButton.Location = new System.Drawing.Point(12, 398);
+            this.userGuideButton.Name = "userGuideButton";
+            this.userGuideButton.Size = new System.Drawing.Size(190, 40);
+            this.userGuideButton.TabIndex = 8;
+            this.userGuideButton.Text = "User guide";
+            this.userGuideButton.UseVisualStyleBackColor = true;
+            this.userGuideButton.Click += new System.EventHandler(this.userGuideButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(98, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(600, 2);
+            this.label1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(98, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(600, 2);
+            this.label2.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox2.Location = new System.Drawing.Point(181, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(418, 46);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Mainframe Reporting";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(98, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(600, 2);
+            this.label3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userGuideButton);
+            this.Controls.Add(this.dataFormatButton);
+            this.Controls.Add(this.reportStatusLabel);
+            this.Controls.Add(this.reportStatusProgress);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browseFilesButton);
+            this.Controls.Add(this.runReportButton);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Mainframe Reporting Script";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runReportButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseFilesButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar reportStatusProgress;
+        private System.ComponentModel.BackgroundWorker reportBgWorker;
+        private System.Windows.Forms.Label reportStatusLabel;
+        private System.Windows.Forms.Button dataFormatButton;
+        private System.Windows.Forms.Button userGuideButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MainframeReportScript/start/Main_Form.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace start
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void runReportButton_Click(object sender, EventArgs e)
+        {
+
+            
+            if (reportBgWorker.IsBusy != true)
+            {
+                reportStatusProgress.Maximum = 100;
+                reportStatusProgress.Step = 1;
+                reportStatusProgress.Value = 0;
+                reportStatusLabel.Text = "Running";
+                // Start the asynchronous operation.
+                reportBgWorker.RunWorkerAsync();
+            }
+        }
+
+        private void reportBgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var backgroundWorker = sender as BackgroundWorker;
+            backgroundWorker.ReportProgress(0);
+            
+            backgroundWorker.ReportProgress(10);
+            
+            string fileName = textBox1.Text;
+
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+
+            try
+            {
+                oXL = new Excel.Application();
+
+                //Get existing workbook
+                oWB = (Excel._Workbook)(oXL.Workbooks.Open(fileName));
+                var xlSheets = oWB.Sheets as Excel.Sheets;
+
+                //Create APP and vlookup for Warton4 data
+                oSheet = ChangeSheet("Warton4 Data", oWB);
+                InsertAppColumn(oSheet); //Call function to insert column
+
+                //Create pivot for Warton4 data on new sheet
+                var PivotSheet = (Excel.Worksheet)xlSheets.Add(Type.Missing, xlSheets[4], Type.Missing, Type.Missing);
+                PivotSheet.Name = "Warton4 Pivot"; //Name new sheet
+                CreatePivotSheet(oWB, oSheet, PivotSheet, "Warton4 Pivot", false); //Call function to create pivot
+
+                backgroundWorker.ReportProgress(25);
+                
+
+                //Do the same for Brought data
+                oSheet = ChangeSheet("Brought Data", oWB);
+                InsertAppColumn(oSheet);
+                //Create Pivot
+                PivotSheet = (Excel.Worksheet)xlSheets.Add(Type.Missing, xlSheets[4], Type.Missing, Type.Missing);
+                PivotSheet.Name = "Brought Pivot";
+                CreatePivotSheet(oWB, oSheet, PivotSheet, "Brought Pivot", false);
+
+                backgroundWorker.ReportProgress(35);
+
+                //Now CHAD data
+                oSheet = ChangeSheet("CHAD Data", oWB);
+                InsertAppColumn(oSheet);
+                //CHAD Pivot
+                PivotSheet = (Excel.Worksheet)xlSheets.Add(Type.Missing, xlSheets[4], Type.Missing, Type.Missing);
+                PivotSheet.Name = "CHAD Pivot";
+                CreatePivotSheet(oWB, oSheet, PivotSheet, "CHAD Pivot", false);
+
+                backgroundWorker.ReportProgress(50);
+
+                //Warton2 data
+                oSheet = ChangeSheet("Warton2 Data", oWB);
+                InsertAppColumn(oSheet);
+                //Warton2 Pivot
+                PivotSheet = (Excel.Worksheet)xlSheets.Add(Type.Missing, xlSheets[4], Type.Missing, Type.Missing);
+                PivotSheet.Name = "Warton2 Pivot";
+                CreatePivotSheet(oWB, oSheet, PivotSheet, "Warton2 Pivot", false);
+
+                backgroundWorker.ReportProgress(60);
+
+                //Once all pivots are created, create a sheet unifying all data from created pivots,
+                //this is done to prevent excel from reaching the row limit.
+                CreateConsolidatedSheet(oWB, xlSheets, backgroundWorker);
+
+                backgroundWorker.ReportProgress(100);
+
+
+
+                //Give user control of excel spreadsheet once all processing is finished
+                oXL.Visible = true;
+                oXL.UserControl = true;
+            }
+            //Catch any errors that may arise
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+                errorMessage = String.Concat(errorMessage, ". Ensure correct excel file has been used.");
+
+                MessageBox.Show(errorMessage, "Error");
+
+                backgroundWorker.ReportProgress(0);
+            }
+
+        }
+
+        //Change worksheet
+        private Excel._Worksheet ChangeSheet(string sheetName, Excel._Workbook oWB)
+        {
+            
+            if (sheetName == "Warton4 Data")
+                return (Excel.Worksheet)oWB.Worksheets["Warton4 Data"];
+            else if (sheetName == "Brought Data")
+                return (Excel.Worksheet)oWB.Worksheets["Brought Data"];
+            else if (sheetName == "CHAD Data")
+                return (Excel.Worksheet)oWB.Worksheets["CHAD Data"];
+            else if (sheetName == "Warton2 Data")
+                return (Excel.Worksheet)oWB.Worksheets["Warton2 Data"];
+            else
+                return null;
+        }
+
+
+        //Creates pivot table
+        private void CreatePivotSheet(Excel._Workbook workbook, Excel._Worksheet dataSheet, Excel._Worksheet pivotSheet, string tableName, bool unified)
+        {
+            //If consolidated sheet, range selects up until column C, if normal pivot, selects up until K
+            string col;
+            if (unified == true)
+            {
+                col = "C";
+            }
+            else
+            {
+                col = "K";
+            }
+            //Get last used row
+            var lastUsedRow = getLastUsedRow(dataSheet); //Select all data from starting cell to last column + row
+            var dataRange = dataSheet.get_Range("A1", col + lastUsedRow);
+            var pivotRange = pivotSheet.Cells[1, 1]; //Select target location
+            var oPivotCache = (Excel.PivotCache)workbook.PivotCaches().Add(Excel.XlPivotTableSourceType.xlDatabase, dataRange); //Create cache specifying data is coming from a table
+            var oPivotTable = (Excel.PivotTable)pivotSheet.PivotTables().Add(PivotCache: oPivotCache, TableDestination: pivotRange, TableName: tableName);//Create table
+
+            if (unified == true)//If consolidated sheet
+            {
+                //Set Row field to 'APP'
+                var RowPivotField = (Excel.PivotField)oPivotTable.PivotFields("APP");
+                RowPivotField.Orientation = Excel.XlPivotFieldOrientation.xlRowField;
+
+                //Set Values field to 'Total'
+                var SumPivotField = (Excel.PivotField)oPivotTable.PivotFields("Total");
+                SumPivotField.Orientation = Excel.XlPivotFieldOrientation.xlDataField;
+                SumPivotField.Function = Excel.XlConsolidationFunction.xlSum;
+                SumPivotField.Name = "CPU Time";
+
+                //Set Column field to 'LPAR'
+                var ColPivotField = (Excel.PivotField)oPivotTable.PivotFields("LPAR");
+                ColPivotField.Orientation = Excel.XlPivotFieldOrientation.xlColumnField;
+
+            }
+            else //If normal sheet
+            {
+                //Set Row field to 'APP'
+                Excel.PivotField RowPivotField = (Excel.PivotField)oPivotTable.PivotFields("APP");
+                RowPivotField.Orientation = Excel.XlPivotFieldOrientation.xlRowField;
+
+                //Set Values field to 'CPUTIME'
+                Excel.PivotField SumPivotField = (Excel.PivotField)oPivotTable.PivotFields("CPUTIME");
+                SumPivotField.Orientation = Excel.XlPivotFieldOrientation.xlDataField;
+                SumPivotField.Function = Excel.XlConsolidationFunction.xlSum;
+                SumPivotField.Name = "CPU Time";
+            }
+        }
+
+        //Insert column 'APP' for the given worksheet
+        private void InsertAppColumn(Excel._Worksheet oWS)
+        {
+
+            Excel.Range oRng;
+
+
+            oRng = oWS.Columns["D"]; // Select column D
+
+            //Insert column and shift existing columns to the right
+            oRng.EntireColumn.Insert(Excel.XlInsertShiftDirection.xlShiftToRight,
+                Excel.XlInsertFormatOrigin.xlFormatFromRightOrBelow);
+            oWS.Cells[1, 4] = "APP"; //Set header of the column
+
+            //Get the last row in the spreadsheet containing data
+            var lastUsedRow = getLastUsedRow(oWS);
+
+            oRng = oWS.get_Range("D2", "D" + lastUsedRow); //Select all rows in column D below the title
+            oRng.Formula = "=VLOOKUP(C2,Lookup!A:B,2,TRUE)"; //Set each cell to perform a vlookup
+        }
+
+        //Create new column for each pivot containing the corresponding LPAR
+        private void CreateLPAR(Excel._Worksheet oSheet, string sheetName)
+        {
+            //Get the last row in the spreadsheet containing data
+            var lastUsedRow = getLastUsedRow(oSheet);
+
+            var oRng = oSheet.get_Range("C3", "C" + (lastUsedRow - 1)); //Get range of data excluding titles & grand total
+            oRng.Value2 = sheetName; //Change contents to the LPAR
+            oSheet.Cells[2, 3] = "LPAR";//Set title of column
+        }
+
+        //Creates unifying sheet
+        private void CreateConsolidatedSheet(Excel._Workbook workbook, Excel.Sheets xlSheets, BackgroundWorker backgroundWorker)
+        {
+            //Create empty sheet
+            var ConsolidatedSheet = (Excel.Worksheet)xlSheets.Add(Type.Missing, xlSheets[8], Type.Missing, Type.Missing);
+            ConsolidatedSheet.Name = "Consolidated Data";//Rename it
+
+            //Create empty pivot sheet
+            var ConsolidatedPivot = (Excel.Worksheet)xlSheets.Add(Type.Missing, xlSheets[9], Type.Missing, Type.Missing);
+            ConsolidatedPivot.Name = "Consolidated Pivot";
+
+            backgroundWorker.ReportProgress(70);
+
+            //Add LPAR column to all pivot's by calling CreateLPAR()
+            var Warton4Pivot = (Excel.Worksheet)workbook.Sheets[8];
+            CreateLPAR(Warton4Pivot, "Warton4");
+
+            var BroughtPivot = (Excel.Worksheet)workbook.Sheets[7];
+            CreateLPAR(BroughtPivot, "Brought");
+
+            var CHADPivot = (Excel.Worksheet)workbook.Sheets[6];
+            CreateLPAR(CHADPivot, "CHAD");
+
+            var Warton2Pivot = (Excel.Worksheet)workbook.Sheets[5];
+            CreateLPAR(Warton2Pivot, "Warton2");
+
+            //Get the last row in the spreadsheet containing data
+            var lastUsedRow = getLastUsedRow(Warton4Pivot);
+
+            var Warton4Range = Warton4Pivot.get_Range("A2", "C" + (lastUsedRow - 1));//Get range of data including title, excluding grandtotal
+            var ConsRange = ConsolidatedSheet.Range["A1", "C" + (lastUsedRow - 2)];//Get range of cells to copy data too
+            Warton4Range.Copy(ConsRange);//Copy the data
+
+            var startOfNextData = lastUsedRow - 1;//Set where the next set of data needs to be appended to
+
+            //Find new last used row
+            lastUsedRow = getLastUsedRow(BroughtPivot);
+            var BroughtRange = BroughtPivot.get_Range("A3", "C" + (lastUsedRow - 1));//Get data from Brough
+            ConsRange = ConsolidatedSheet.Range["A" + startOfNextData, "C" + (startOfNextData + lastUsedRow - 2)];//Copy target location
+            BroughtRange.Copy(ConsRange);//Move Brought's data
+
+            startOfNextData += lastUsedRow - 3;
+            lastUsedRow = getLastUsedRow(CHADPivot);
+
+            var CHADRange = CHADPivot.get_Range("A3", "C" + (lastUsedRow - 1));
+            ConsRange = ConsolidatedSheet.Range["A" + startOfNextData, "C" + (startOfNextData + lastUsedRow - 2)];
+            CHADRange.Copy(ConsRange);
+            backgroundWorker.ReportProgress(80);
+
+            startOfNextData += lastUsedRow - 3;
+            lastUsedRow = getLastUsedRow(Warton2Pivot);
+
+            var Warton2Range = Warton2Pivot.get_Range("A3", "C" + (lastUsedRow - 1));
+            ConsRange = ConsolidatedSheet.Range["A" + startOfNextData, "C" + (startOfNextData + lastUsedRow - 2)];
+            Warton2Range.Copy(ConsRange);
+
+            backgroundWorker.ReportProgress(90);
+
+            //Now create Pivot for the unified data using the unified sheet
+            CreatePivotSheet(workbook, ConsolidatedSheet, ConsolidatedPivot, "Consolidated Pivot", true);
+            FormatConsolidatedSheet(workbook, ConsolidatedPivot);
+            CreateChart(ConsolidatedPivot, backgroundWorker);
+        }
+
+        private void FormatConsolidatedSheet(Excel._Workbook workbook, Excel._Worksheet consolidatedPivot)
+        {
+            Excel.Range oRng = consolidatedPivot.get_Range("A1", "F2");
+            oRng.Interior.Color = ColorTranslator.ToOle(Color.LightBlue);
+            oRng.Borders.LineStyle = Excel.XlLineStyle.xlLineStyleNone;
+
+            oRng = consolidatedPivot.get_Range("A3", "F" + getLastUsedRow(consolidatedPivot));
+            oRng.Interior.Color = ColorTranslator.ToOle(Color.White);
+        }
+
+        private void CreateChart(Excel._Worksheet oSheet, BackgroundWorker backgroundWorker)
+        {
+            //Connect to workbook
+            Excel._Workbook oWB = (Excel._Workbook)oSheet.Parent;
+
+            //Get range of data in which the chart will reference to
+            Excel.Range oRng = oSheet.get_Range("A1", "C" + getLastUsedRow(oSheet));
+
+            //Create chart using ChartWizard
+            Excel._Chart oChart = (Excel._Chart)oWB.Charts.Add(Missing.Value, Missing.Value,
+                Missing.Value, Missing.Value);
+            oChart.ChartWizard(oRng, Excel.XlChartType.xlColumnStacked, Missing.Value,
+                Excel.XlRowCol.xlRows, Missing.Value, Missing.Value, Missing.Value,
+                Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            oChart.Location(Excel.XlChartLocation.xlLocationAsObject, oSheet.Name);
+
+            //Set location, height and width of the chart
+            oSheet.Shapes.Item(1).Top = 75;
+            oSheet.Shapes.Item(1).Left = 600;
+            oSheet.Shapes.Item(1).Width = 1304;
+            oSheet.Shapes.Item(1).Height = 419;
+
+            backgroundWorker.ReportProgress(95);
+        }
+
+        //Returns last used row
+        private int getLastUsedRow(Excel._Worksheet oSheet)
+        {
+            return oSheet.Cells.Find("*", Missing.Value,
+                               Missing.Value, Missing.Value,
+                               Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious,
+                               false, Missing.Value, Missing.Value).Row;
+        }
+
+        private void reportBgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            reportStatusProgress.Value = e.ProgressPercentage;
+
+        }
+        // This event handler deals with the results of the background operation.
+        private void reportBgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                reportStatusLabel.Text = "Canceled!";
+            }
+            else if (e.Error != null)
+            {
+                reportStatusLabel.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                reportStatusLabel.Text = "Finished";
+            }
+        }
+        private void browseFilesButton_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName; //Save filepath to variable
+                try
+                {
+                    //Set textbox to the filepath
+                    textBox1.Text = file;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void dataFormatButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rules for data:\n4 Data sheets required, each with names:\n\'Brought Data\', \'CHAD Data\', \'Warton2 Data\', \'Warton4 Data\'.\nLookup table must be included in the same excel workbook with name,\n\'Lookup\'", "Data Format");
+        }
+
+        private void userGuideButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Instructions:\n1.Browse files and upload Mainframe Data.\n2.Run Report. ~2 mins\n3.Once finished, edit any charts as needed.\n4.Save & upload to sharepoint under filename:\n\t-\'Top 25 Batch Utilisation April 2019.\'\n\t(current month)", "Instructions");
+        }
+
+
+    }
+}
